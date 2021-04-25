ed678d8d843de52f20d48cc2b9cfa0214b28c19d
Ilya Novak <rickmortyand4@gmail.com>
2021-04-25 13:38:57 +0300

    Fix api url

--- src/js/data.js
+++ src/js/data.js
@@ -4,7 +4,7 @@
  function getScores() {
     $.ajax({
         type: 'GET',
-        url:  'http://localhost:3000/scores',
+        url:  'http://157.230.98.200/snake/scores',
     }).done(function(result) {
         scores.innerHTML += "<div>" + result + "</div>";
     }).fail(function (jqXHR, exception) {
