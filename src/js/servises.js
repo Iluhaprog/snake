e6b430a8600b376057237080d86aad81ccefae92
Ilya Novak <rickmortyand4@gmail.com>
2022-02-16 14:31:07 +0300

    feat: Setup project building with webpack.

--- src/js/servises.js
+++
@@ -1,7 +0,0 @@
-function displayEl(el, visible) {
-    el.style.display = visible;
-}
-
-function rnd(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Snake",
+			filename: "index.html",
+			template: "./src/template.html",
+			inject: false,
+		}),
+		new MiniCssExtractPlugin(),
+		new ESLintPlugin(),
+	],
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./dist",
+		port: 3000,
+	},
+	output: {
+		filename: "game.bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+		publicPath: "/",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+};
